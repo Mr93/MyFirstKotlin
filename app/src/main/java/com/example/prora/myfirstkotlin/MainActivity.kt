3be7211f2188decaa6bdb062c0768c9811773702
mamram93 <prorasengan.neu@gmail.com>
2018-03-27 15:33:26 +0700

    adding class fragment

--- app/src/main/java/com/example/prora/myfirstkotlin/MainActivity.kt
+++ app/src/main/java/com/example/prora/myfirstkotlin/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.prora.myfirstkotlin
 
-import android.support.v7.app.AppCompatActivity
+import android.app.FragmentManager
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.prora.myfirstkotlin.fragments.NewsFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            changeFragment(NewsFragment())
+        }
+    }
+
+    fun changeFragment(f: android.support.v4.app.Fragment, cleanStack: Boolean = false) {
+        var fragmentTransaction = supportFragmentManager.beginTransaction()
+        if (cleanStack) {
+            clearBackStack()
+        }
+        fragmentTransaction.setCustomAnimations(
+                R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+        fragmentTransaction.replace(R.id.activity_base_content, f)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    fun clearBackStack() {
+        var fragmentManager = supportFragmentManager
+        if (fragmentManager.backStackEntryCount > 0) {
+            var first = fragmentManager.getBackStackEntryAt(0)
+            fragmentManager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+    }
+
+    override fun onBackPressed() {
+        val fragmentManager = supportFragmentManager
+        if (fragmentManager.backStackEntryCount > 1) {
+            fragmentManager.popBackStack()
+        } else {
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/prora/myfirstkotlin/fragments/NewsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.prora.myfirstkotlin.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.prora.myfirstkotlin.R
+import com.example.prora.myfirstkotlin.commons.inflate
+
+
+class NewsFragment : Fragment() {
+    private var newsList: RecyclerView? = null
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = container?.inflate(R.layout.news_fragment)
+        newsList = view?.findViewById(R.id.news_list)
+        newsList?.setHasFixedSize(true)
+        newsList?.layoutManager = LinearLayoutManager(context)
+        return view
+    }
+}
